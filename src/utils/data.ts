88cc9e3a5a1815c8dfde18e4cdf681e3a5ad3594
DominicDev <s0l0estudi4r@gmail.com>
2026-02-20 00:16:08 +0100

    v0.3.8

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -138,6 +138,12 @@ export const arrayCards = [
     localImage: '/assets/scroll-grow-effect.avif',
     id: '023'
   },
+  {
+    title: 'Practicing Animations',
+    repo: 'https://d960960gm.wixstudio.com/practice-animations',
+    localImage: '/assets/practicing-animations.avif',
+    id: '024'
+  },
 ];
 
 export const TITLE = `${arrayCards.length} Projects using Wix Studio`;

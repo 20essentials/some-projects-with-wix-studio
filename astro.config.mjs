820e33529b2f6618e0074f3e9f5a3ba01de6d15c
DominicDev <s0l0estudi4r@gmail.com>
2026-03-14 00:16:57 +0100

    v0.5.7

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,9 +13,9 @@ export default defineConfig({
 
   //Localhost
   //Deploy normal project
-  // base: '/some-projects-with-wix-studio',
-  // site: 'https://20essentials.github.io/some-projects-with-wix-studio/',
-  site: 'http://localhost:4321/',
+  base: '/some-projects-with-wix-studio',
+  site: 'https://20essentials.github.io/some-projects-with-wix-studio/',
+  // site: 'http://localhost:4321/',
 
   vite: {
     plugins: [tailwindcss()]

504f028f45dde6a00b8e8bdbda10b0aa5adc7bf2
DominicDev <s0l0estudi4r@gmail.com>
2026-03-30 20:12:38 +0200

    v0.7.0

--- src/components/infinite-menu/ContainerInfiniteMenu.tsx
+++ src/components/infinite-menu/ContainerInfiniteMenu.tsx
@@ -5,12 +5,14 @@ import { arrayCards } from '@/utils/data';
 export const ContainerInfiniteMenu = () => {
   return (
     <InfiniteMenu
-      items={arrayCards.map(({ id, localImage, repo, title }, i) => ({
-        image: baseUrl(localImage),
-        title: `Project ${i + 1}`,
-        link: repo,
-        description: title
-      }))}
+      items={arrayCards
+        .toSorted(() => Math.random() - 0.5)
+        .map(({ id, localImage, repo, title }, i) => ({
+          image: baseUrl(localImage),
+          title: `Project ${i + 1}`,
+          link: repo,
+          description: title
+        }))}
     />
   );
 };

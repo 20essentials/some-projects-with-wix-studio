b4301845c1e4be05f56aa90542baf3195be1fcc3
DominicDev <s0l0estudi4r@gmail.com>
2026-04-04 00:23:16 +0200

    v0.7.7

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -372,6 +372,12 @@ export const arrayCards = [
     localImage: '/assets/spotlight-mouse.avif',
     id: '062'
   },
+  {
+    title: 'Simple Blur Effect',
+    repo: 'https://d960960gm.wixstudio.com/projects/blur',
+    localImage: '/assets/blur.avif',
+    id: '063'
+  },
 ];
 
 export const TITLE = `${arrayCards.length} Projects using Wix Studio`;

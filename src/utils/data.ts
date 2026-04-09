b58eae872243069315f7b32ae76a9f5234410f91
DominicDev <s0l0estudi4r@gmail.com>
2026-04-09 16:27:47 +0200

    v0.9.7

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -402,6 +402,12 @@ export const arrayCards = [
     localImage: '/assets/tabs.avif',
     id: '067'
   },
+  {
+    title: 'Burst Hover Animation',
+    repo: 'https://d960960gm.wixstudio.com/projects/burst-hover-animation',
+    localImage: '/assets/burst-hover-animation.avif',
+    id: '068'
+  },
 ];
 
 export const TITLE = `${arrayCards.length} Projects using Wix Studio`;

dd27c1257d6fd93d4eb13a2a20e15a4ff3e9b896
DominicDev <s0l0estudi4r@gmail.com>
2026-07-03 16:21:49 +0200

    v1.5.7

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -901,6 +901,12 @@ export const arrayCards = [
     localImage: '/assets/reveal-image-and-line-by-scroll.avif',
     id: '150'
   },
+  {
+    title: 'Minimalist Wix Pro Gallery',
+    repo: 'https://d960960gm.wixstudio.com/minimalist-gallery/minimalist-gallery',
+    localImage: '/assets/minimalist-gallery.avif',
+    id: '151'
+  },
 ];
 
 export const TITLE = `${arrayCards.length} Projects using Wix Studio`;

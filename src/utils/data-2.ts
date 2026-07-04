df33f3166b9e97621944dbec4032245c39c68230
DominicDev <s0l0estudi4r@gmail.com>
2026-07-04 17:31:54 +0200

    v1.6.2

---
+++ src/utils/data-2.ts
@@ -0,0 +1,15 @@
+export const data2 = [
+  {
+    title: 'Stagger Item Grow',
+    repo: 'https://d960960gm.wixstudio.com/conl3vel',
+    localImage: '/assets/grow-effect.avif',
+    id: '201'
+  },
+  {
+    title: 'Rotate and Move Ball with Scroll',
+    repo: 'https://d960960gm.wixstudio.com/conl3vel',
+    localImage: '/assets/rotate-and-move-ball-with-scroll.avif',
+    id: '202'
+  },
+ 
+];
